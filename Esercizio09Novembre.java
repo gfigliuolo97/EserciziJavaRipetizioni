3e07560e30f372f132df67df88aeafaeca7ecb63
Giovanni Figliuolo <37793621+gfigliuolo97@users.noreply.github.com>
2020-11-09 22:03:49 +0100

    Esercizio 09 Novembre

---
+++ Esercizio09Novembre.java
@@ -0,0 +1,13 @@
+/*
+Traccia:
+Dati N numeri in input, stampare quanti sono dispari e con 4 cifre.
+*/
+class Esercizio09Novembre{
+    
+    public static void main(String[] args){
+
+
+
+
+    }
+}
