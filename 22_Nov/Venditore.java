852dc67968661f57dafa799ff3ae441ae820b1c0
Giovanni Figliuolo <37793621+gfigliuolo97@users.noreply.github.com>
2020-11-22 13:35:53 +0100

    Nuova traccia

---
+++ 22_Nov/Venditore.java
@@ -0,0 +1,53 @@
+/*
+*Traccia
+*Un venditore inserisce l'importo delle vendite di biscotti e taralli.
+*Se vende più di 5.000€ di biscotti riceve una provvigione del 15%, altrimenti del 3%.
+*Se vende più di 10.000€ di taralli riceve una provvigione del 16%, altrimenti del 5%.
+*Se il totale delle vendite e' maggiore di 30.000€, riceve un premio extra del 12%.
+*Stampare a video gli importi delle provvigioni, l'importo del totale delle provvigioni e dell'eventuale premio extra.
+*/
+import java.util.Scanner;
+public class Venditore {
+	public static void main(String[] args) {
+		Scanner tastiera = new Scanner(System.in);
+		double totale_provvigione = 0;
+        double provvigione_biscotti = 0;
+		double provvigione_taralli = 0;
+        double premio_extra = 0;
+
+        //Biscotti
+		System.out.println("Inserisci l'importo delle vendite dei biscotti:");
+		int totale_biscotti = tastiera.nextInt();
+		
+        //Taralli
+        System.out.println("Inserisci l'importo delle vendite dei biscotti:");
+		int totale_taralli = tastiera.nextInt();
+
+        if(totale_biscotti > 5000){
+            provvigione_biscotti = (totale_biscotti * 15)/100;
+        }else{
+            provvigione_biscotti = (totale_biscotti * 3)/100;
+        }
+        
+        System.out.println("Il totale delle provvigioni sui taralli e':" + provvigione_taralli) + "euro.";
+
+        if(totale_taralli > 5000){
+            provvigione_taralli = (totale_taralli * 16)/100;
+        }else{
+            provvigione_taralli = (totale_taralli * 5)/100;
+        }
+
+        System.out.println("Il totale delle provvigioni sui biscotti e':" + provvigione_biscotti + "euro.");
+		
+		totale_provvigione = provvigione_biscotti + provvigione_taralli;
+
+        if(totale_provvigione > 30000){
+            premio_extra = (totale_provvigione * 12) / 100;
+            totale_provvigione = totale_provvigione + premio_extra;
+            System.out.println("Hai ricevuto un premio extra del valore di:" + premio_extra + "euro.");
+        }
+
+        System.out.println("Il totale delle provvigioni e del premio extra e':" + totale_provvigione + "euro.");
+
+	}
+}
