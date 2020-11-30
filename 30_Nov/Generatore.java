f389ce4d87fe6c1044d0259b61b25c3acff85a30
Giovanni Figliuolo <37793621+gfigliuolo97@users.noreply.github.com>
2020-11-30 23:37:24 +0100

    Creazione di un numero intero utilizzando la classe Random

--- 30_Nov/Generatore.java
+++ 30_Nov/Generatore.java
@@ -1,15 +1,23 @@
+import java.util.Random;
 
 public class Generatore {
 
 	public static void main(String[] args) {
 		double numeroCasuale = Math.random();
-		
 		//Viene stampato un numero double compreso tra 0 e 1 (estremi inclusi).
 		System.out.println("Numero generato:" + numeroCasuale);
 
 		double numeroCasualeMinore5 = Math.random()*5;
-		//Viene stampato un numero double compreso tra 0 e 5 (estremi inclusi).
+		//Viene stampato un numero double compreso tra 0 e 5.
 		System.out.println("Numero generato:" + numeroCasualeMinore5);
+		
+		
+		//Utilizzando un oggetto della classe Random
+		Random random = new Random();
+		
+		//Generiamo un numero casuale intero compreso tra 0 e 9.
+		int numero = random.nextInt(10);
+		System.out.println(numero);
 	}
 
 }
