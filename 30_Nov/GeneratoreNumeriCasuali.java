bdbc3eeb5e9946014278aa06af6c91939bb7787c
Giovanni Figliuolo <37793621+gfigliuolo97@users.noreply.github.com>
2020-11-30 23:28:21 +0100

    New esercise

---
+++ 30_Nov/GeneratoreNumeriCasuali.java
@@ -0,0 +1,11 @@
+
+public class Generatore {
+
+	public static void main(String[] args) {
+		double numeroCasuale = Math.random();
+		
+		//Viene stampato un numero double compreso tra 0 e 1 (estremi inclusi).
+		System.out.println("Numero generato:" + numeroCasuale);
+	}
+
+}
---
+++ 30_Nov/README.md
@@ -0,0 +1,2 @@
+# Generatore di Numeri Casuali
+Viene generato un numero con la virgola (double) compreso tra 0 e 1 (compresi).
