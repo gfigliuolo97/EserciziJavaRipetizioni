352085da75332239e01537e52e547e385dab5e47
Giovanni Figliuolo <37793621+gfigliuolo97@users.noreply.github.com>
2020-11-30 23:32:53 +0100

    Creazione di un numero compreso tra 0 e 5

--- 30_Nov/Generatore.java
+++ 30_Nov/Generatore.java
@@ -6,6 +6,10 @@ public class Generatore {
 		
 		//Viene stampato un numero double compreso tra 0 e 1 (estremi inclusi).
 		System.out.println("Numero generato:" + numeroCasuale);
+
+		double numeroCasualeMinore5 = Math.random()*5;
+		//Viene stampato un numero double compreso tra 0 e 5 (estremi inclusi).
+		System.out.println("Numero generato:" + numeroCasualeMinore5);
 	}
 
 }
