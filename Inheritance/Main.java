853a96d1f4c07256a29c7751d1692f0912b1925e
Giovanni Figliuolo <37793621+gfigliuolo97@users.noreply.github.com>
2020-12-01 17:31:08 +0100

    Added Main class

---
+++ Inheritance/Main.java
@@ -0,0 +1,15 @@
+package School;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Teacher t1 = new Teacher("Fanizzi","Nicola","ICSE");
+		System.out.println(t1);
+		
+		Student s1 = new Student("Figliuolo", "Giovanni", 80);
+		System.out.println(s1);
+	
+	}
+
+}
